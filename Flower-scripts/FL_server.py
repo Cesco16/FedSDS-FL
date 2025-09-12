acd28b3c827dee959a4dbcf50ed9dbe347c8ee17
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2025-09-12 09:04:16 +0200

    Update FL_server.py

--- Flower-scripts/FL_server.py
+++ Flower-scripts/FL_server.py
@@ -1,32 +1,24 @@
 
-# Author: Francesco Casadei
-# Email: francesco.casadei20@unibo.it
-# Date: 19/02/2024
+# Author: Francesco Casadei and Luciana Carota
+# Email: francesco.casadei20@unibo.it, luciana.carota@unibo.it
+# Date: 19/05/2025
 
 # Import libraries
 import pickle
 import flwr as fl
 import sys
 
-connection_port = 8005 #8437
+connection_port = '8005'
+ip = '000.00.00.00'
 
-print(connection_port)
-
-
-##file_name = './DeepSurv_cv_val_MDS_platform_1nodes_v2.pk'
-##file_name = './DeepSurv_cv_uniquetest_MDS_platform_1nodes.pk'
-##file_name = './DeepSurv_cv_val_MDS_platform_1nodes_v2.pk'
 file_name = './DeepSurv_cv_mds.pk'
-##file_name = './DS_uniquetest_MDS_platform_v1_noinit.pk'
 
 with open(file_name, "rb") as f:
     model = pickle.load(f).create_strategy()
 
 # Start Flower server
 fl.server.start_server(
-#    server_address="10.255.219.120:8082",
-    server_address=str('172.17.34.44:'+str(connection_port)),
-    #server_address=str('137.204.51.140:'+str(connection_port)),
+    server_address=str(ip+':'+ connection_port),
     config=fl.server.ServerConfig(num_rounds=100),
     strategy=model
 )
