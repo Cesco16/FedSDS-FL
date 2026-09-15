ba7e0aa0061699b674dc7eaf566bac80699d59bd
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2026-09-15 15:08:17 +0200

    Update main_ds_LGG.py

--- Flower-scripts/main_ds_LGG.py
+++ Flower-scripts/main_ds_LGG.py
@@ -14,12 +14,6 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-# ============================================================================
-# CONFIGURAZIONE DELLA STRATEGY DI FEDERATED LEARNING
-# Cambia questi due valori per passare da FedAvg a FedProx o FedNova.
-# Puoi anche sovrascrivere STRATEGY_NAME da riga di comando lanciando:
-#   python FL_server.py fedprox     (oppure: fedavg | fednova)
-# ============================================================================
 STRATEGY_NAME = "fedprox"   # "fedavg" | "fedprox" | "fednova"
 PROXIMAL_MU = 0.6#0.1           # usato solo se STRATEGY_NAME == "fedprox"
 
